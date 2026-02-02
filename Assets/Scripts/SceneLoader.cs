a138e1b3c8278d6485c2b4c4b387fca6c49fa189
Kaylinhh <ly.julie@live.fr>
2026-02-02 19:35:46 +0100

    Update script to fit WebGL build

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class SceneLoader : MonoBehaviour // handles scene navigation
 {
+
+    public Button quitButton;
+
     public void GoToScene(string sceneName)
     {
         SceneTransitionManager.instance.LoadScene(sceneName);
@@ -14,9 +19,13 @@ public class SceneLoader : MonoBehaviour // handles scene navigation
     {
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
+#elif UNITY_WEBGL
+    quitButton.gameObject.SetActive(false);
+    Debug.Log("Quit not supported in WebGL. Close the tab to exit.");
 #else
-        Application.Quit();
-        Debug.Log("You've closed the game.");
+    Application.Quit();
+    Debug.Log("You've closed the game.");
 #endif
     }
+
 }
